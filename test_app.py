1544ba85d811f312d4a6cace60e2376e4f7aaab6
aghilas <aghilasouldbraham8@gmail.com>
2025-03-16 04:25:26 +0000

    test_app final

--- test_app.py
+++ test_app.py
@@ -1,6 +1,32 @@
-from app import product_service
+import pytest
+from flask import json
+from app import product_service  # depuis app.py
 
-def test_home() :
-    reponse = product_service.test_home().get("/")
+@pytest.fixture
+def client():
+    #Créer un client de test Flask
+    return product_service.test_client()
 
-    assert reponse.status_code == 200
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Bienvenue sur le service des produits" in response.data
+
+def test_get_products(client):
+    response = client.get("/product")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
+    assert len(data) >= 1  # si il y a au moins 1 produit
+
+# teste POST
+def test_post_product(client):
+    new_product = {"id": 3, "name": "MacBook Pro", "price": 2500}
+    response = client.post("/product", data=json.dumps(new_product), content_type="application/json")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data["message"] == "Product added"
+    assert data["product"] == new_product
+
+if __name__ == "__main__":
+    pytest.main()
